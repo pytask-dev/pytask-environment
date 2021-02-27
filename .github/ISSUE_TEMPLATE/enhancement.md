e976ff4e81513dc62a5ceaa6595f60700267391c
Tobias Raabe <raabe@posteo.de>
2021-02-27 11:38:53 +0100

    Prepare pytask-environment to be published on PyPI. (#3)

--- .github/ISSUE_TEMPLATE/enhancement.md
+++ .github/ISSUE_TEMPLATE/enhancement.md
@@ -1,7 +1,7 @@
 ---
 
 name: Enhancement
-about: Suggest an idea for pytask-latex
+about: Suggest an idea for pytask-environment
 title: "ENH:"
 labels: "enhancement"
 
@@ -9,7 +9,7 @@ labels: "enhancement"
 
 #### Is your feature request related to a problem?
 
-Provide a description of what the problem is, e.g. "I wish I could use pytask-latex
+Provide a description of what the problem is, e.g. "I wish I could use pytask-environment
 to do [...]".
 
 #### Describe the solution you'd like
--- setup.py
+++ setup.py
@@ -1,10 +1,47 @@
+from pathlib import Path
+
 from setuptools import find_packages
 from setuptools import setup
 
+import versioneer
+
+README = Path("README.rst").read_text()
+
+PROJECT_URLS = {
+    "Documentation": "https://github.com/pytask-dev/pytask-environment",
+    "Github": "https://github.com/pytask-dev/pytask-environment",
+    "Tracker": "https://github.com/pytask-dev/pytask-environment/issues",
+    "Changelog": "https://github.com/pytask-dev/pytask-environment/blob/main/"
+    "CHANGES.rst",
+}
+
+
 setup(
     name="pytask-environment",
-    version="0.0.1",
+    version=versioneer.get_version(),
+    cmd_class=versioneer.get_cmdclass(),
+    description="Detect changes in your pytask environment and abort a project build.",
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    author="Tobias Raabe",
+    author_email="raabe@posteo.de",
+    python_requires=">=3.6",
+    url=PROJECT_URLS["Github"],
+    project_urls=PROJECT_URLS,
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    platforms="any",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     entry_points={"pytask": ["pytask_environment = pytask_environment.plugin"]},
+    zip_false=False,
 )
