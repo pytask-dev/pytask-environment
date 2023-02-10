bea71b02db77963d5ed6907ae7841127e6cc2a58
Tobias Raabe <raabe@posteo.de>
2023-02-10 15:51:58 +0100

    Make the package compatible with pytask v0.3." (#22)

--- src/pytask_environment/database.py
+++ src/pytask_environment/database.py
@@ -2,7 +2,13 @@
 from __future__ import annotations
 
 from pony import orm
-from pytask import db
+
+
+# Can be removed with pytask v0.4.
+try:
+    from pytask import db
+except ImportError:
+    from _pytask.database_utils import db
 
 
 class Environment(db.Entity):
